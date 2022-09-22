ebffb9be89427cb59d30e32d01d9a14e6c236b7d
praveenmethraskar <praveenmethraskar@gmail.com>
2022-09-22 18:14:40 +0530

    main branch welcome message

--- IndianStateCensusAnalyser/Program.cs
+++ IndianStateCensusAnalyser/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State analyser");
+        }
+    }
+}
